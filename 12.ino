3d999f005749bd5e3b94022e112ac69b899b0d06
Gray-Sword <seifhackclub@gmail.com>
2024-11-11 16:35:20 +0000

    Add files via upload

---
+++ 12.ino
@@ -0,0 +1,30 @@
+#include<LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; 
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7); 
+
+float celsius;
+int temp = A1;
+int fan = 7;
+
+void setup(){
+	pinMode(temp,INPUT);
+    pinMode(fan,OUTPUT);
+}
+
+
+void loop(){
+
+  digitalWrite(7, HIGH);
+  
+  celsius = analogRead(temp)*0.004882814;
+  	celsius = (celsius - 0.5) * 100.0;
+  	
+  	lcd.setCursor(0,1);
+	lcd.print("Temp: ");
+  	lcd.print(celsius);
+	lcd.print(" C");
+  	delay(1000);
+  	lcd.clear();
+
+}
